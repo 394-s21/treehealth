a63017e37b59653acce20e419db7b18c13e98b1b
siwoopark10 <57846202+siwoopark10@users.noreply.github.com>
2021-05-12 12:06:12 -0500

    implemented hooks and refactored code

--- plots/SapFlow.js
+++ plots/SapFlow.js
@@ -1,124 +1,94 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Text, StyleSheet, TextInput, TouchableHighlight, ScrollView, SafeAreaView, StatusBar } from 'react-native';
 import { ListItem, Icon } from 'react-native-elements'
 import { NavigationContainer, CommonActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
 import { Foundation } from '@expo/vector-icons';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 import Charts from './Charts';
 
 
-export default class SapFlow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDropdown = this.handleDropdown.bind(this);
-    this.state = {
-      treeSpecies: 'American Elm',
-      treeLocation: '4100 IL-53, Lisle',
-      treeDiameter: 'Diameter: 3ft',
-      treeSensor: 'Sensor: e7vi3',
-      average: '8.5',
-      selectedView: 'daily',
-      selectedTime: 'Mar 1',
-      days: ['Mar 1', 'Mar 2', 'Mar 3', 'Mar 4', 'Mar 5'],
-      weeks: ['Feb 28 - Mar 6', 'Mar 7-13', 'Mar 14-20'],
-      months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'],
+export default function SapFlow({ navigation }) {
+  const treeInfo = {
+    treeSpecies: 'American Elm',
+    treeLocation: '4100 IL-53, Lisle',
+    treeDiameter: 'Diameter: 3ft',
+    treeSensor: 'Sensor: e7vi3',
+    average: '8.5'
+  }
+  const [selectedView, setSelectedView] = useState('daily')
+  const [selectedTime, setSelectedTime] = useState('Mar 1')
+  const days = ['Mar 1', 'Mar 2', 'Mar 3', 'Mar 4', 'Mar 5']
+  const weeks = ['Feb 28 - Mar 6', 'Mar 7-13', 'Mar 14-20']
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  function DropdownTwo() {
+    var timeRange = days
+    if (selectedView == "daily") {
+      timeRange = days
+    } else if (selectedView == "weekly") {
+      timeRange = weeks
+    } else if (selectedView == "monthly") {
+      timeRange = months
     }
 
-  }
-
-  handleDropdown(itemValue) {
-    this.setState({selectedView: itemValue}, () => this.returnDropdownTwo());
-  }
-
-  handleDropdownTwo(itemValue) {
-    this.setState({selectedTime: itemValue});
-  }
-
-  returnDropdownTwo() {
-    if (this.state.selectedView == "daily"){
+    
       return (
         <Picker
-              selectedValue={this.state.selectedTime}
-              style={{ height: 45, width: vw(40), backgroundColor: '#38735D', color: 'white', borderRadius: 5, padding: 10 }}
-              onValueChange={(selectedValue) => this.handleDropdownTwo(selectedValue)}
-            >
-              {this.state.days.map((item, id) => (
-                  <Picker.Item key={item, id} label={item} value={item} />
-              ))}
-            </Picker>  
+          selectedValue={selectedTime}
+          style={{ height: 45, width: vw(40), backgroundColor: '#38735D', color: 'white', borderRadius: 5, padding: 10 }}
+          onValueChange={setSelectedTime}
+        >
+          {timeRange.map((item, id) => (
+            <Picker.Item key={item, id} label={item} value={item} />
+          ))}
+        </Picker>
       )
-    } else if (this.state.selectedView == "weekly"){
-      return (
-        <Picker
-              selectedValue={this.state.selectedTime}
-              style={{ height: 45, width: vw(40), backgroundColor: '#38735D', color: 'white', borderRadius: 5, padding: 10 }}
-              onValueChange={(selectedValue) => this.handleDropdownTwo(selectedValue)}
-            >
-              {this.state.weeks.map((item) => (
-                  <Picker.Item key={item} label={item} value={item} />
-              ))}
-            </Picker>  
-      )
-    } else {
-      return (
-        <Picker
-              selectedValue={this.state.selectedTime}
-              style={{ height: 45, width: vw(40), backgroundColor: '#38735D', color: 'white', borderRadius: 5, padding: 10 }}
-              onValueChange={(selectedValue) => this.handleDropdownTwo(selectedValue)}
-            >
-              {this.state.months.map((item) => (
-                  <Picker.Item key={item} label={item} value={item} />
-              ))}
-            </Picker>  
-      )
-    }
   }
 
-  render() {
-    let dropdownTwo = this.returnDropdownTwo();
-    return (
+  return (
     <View style={styles.container}>
       <Text style={styles.filler}></Text>
       <Text style={styles.paragraph}>
-        <Charts style = {styles.plot}/>
+        <Charts style={styles.plot} />
         {/*<View style={styles.plot}></View>*/}
-        <View style={{textAlign: 'center', marginBottom: 10}}>
+        <View style={{ textAlign: 'center', marginBottom: 10 }}>
           <View style={styles.filterDiv}>
             <Picker
-              selectedValue={this.state.selectedView}
+              selectedValue={selectedView}
               style={{ height: 45, width: vw(40), backgroundColor: '#38735D', color: 'white', borderRadius: 5, padding: 10 }}
-              onValueChange={(itemValue) => this.handleDropdown(itemValue)}
+              onValueChange={setSelectedView}
             >
               <Picker.Item label="daily" value="daily" />
               <Picker.Item label="weekly" value="weekly" />
               <Picker.Item label="monthly" value="monthly" />
             </Picker>
-            <View>{dropdownTwo}</View>
+            <View><DropdownTwo /></View>
           </View>
         </View>
         <Text style={styles.paragraph}>
         </Text>
         <View style={styles.info}>
           <View style={styles.add}>
-                    <Text> {this.state.treeSpecies}</Text>
-                    <Text> {this.state.treeDiameter}</Text>
-                    <Text> {this.state.treeSensor}</Text>
-                    <Text> {this.state.treeLocation}</Text>
+            <Text> {treeInfo.treeSpecies}</Text>
+            <Text> {treeInfo.treeDiameter}</Text>
+            <Text> {treeInfo.treeSensor}</Text>
+            <Text> {treeInfo.treeLocation}</Text>
           </View>
           <View style={styles.add}>
-                    <Text style={styles.inline}>
-                      <Text style={{fontSize: 30, fontWeight: 'bold', marginRight: 5}}>{this.state.average}</Text>
-                      <Text>cm/hr</Text>
-                    </Text>
-                    <Text >Average</Text>
+            <Text style={styles.inline}>
+              <Text style={{ fontSize: 30, fontWeight: 'bold', marginRight: 5 }}>{treeInfo.average}</Text>
+              <Text>cm/hr</Text>
+            </Text>
+            <Text >Average</Text>
           </View>
         </View>
       </Text>
     </View>
   );
-}}
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -133,11 +103,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   info: {
-    width: vw(90), 
-    flexDirection: 'row', 
-    textAlign: 'left', 
-    margin: vh(1), 
-    justifyContent: 'space-between', 
+    width: vw(90),
+    flexDirection: 'row',
+    textAlign: 'left',
+    margin: vh(1),
+    justifyContent: 'space-between',
     flex: 7
   },
   add: {
