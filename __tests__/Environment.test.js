e9271fc4a6eccfcbcdef30da5199e946cfbcec09
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-06-05 18:04:39 -0500

    Add start of new test

---
+++ __tests__/Environment.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import SapFlow from '../plots/Environment';
+
+it('renders environment chart', () => {
+  expect(true).toBe(true);
+  // const { getByText } = render(<Environment />);
+  // expect(getByText('VictoryChart').toBeTruthy());
+});
